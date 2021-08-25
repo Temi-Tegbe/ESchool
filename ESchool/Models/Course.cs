a3bec34d61577d5b4abfb3836880463382957665
Temi-Tegbe <temiloluwat@gmail.com>
2021-08-25 11:11:37 +0100

    added Teacher Controller

---
+++ ESchool/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESchool.Models
+{
+    public class Course
+    {
+
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public int Unts { get; set; }
+    }
+}
